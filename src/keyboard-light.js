94ac6e9a85a03f172b0b9b5606c30ae76eab70e6
Luander <69652684+LuanderFarias@users.noreply.github.com>
2021-07-05 10:50:37 -0300

    Add files via upload

---
+++ src/keyboard-light.js
@@ -0,0 +1,328 @@
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 81) {
+       var x = document.getElementsByClassName("q");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 87) {
+       var x = document.getElementsByClassName("w");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 69) {
+       var x = document.getElementsByClassName("e");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 82) {
+       var x = document.getElementsByClassName("r");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 84) {
+       var x = document.getElementsByClassName("t");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 89) {
+       var x = document.getElementsByClassName("y");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 85) {
+       var x = document.getElementsByClassName("u");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 73) {
+       var x = document.getElementsByClassName("i");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    } 
+
+    else if(event.keyCode == 79) {
+       var x = document.getElementsByClassName("o");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 80) {
+       var x = document.getElementsByClassName("p");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 65) {
+       var x = document.getElementsByClassName("a");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 83) {
+       var x = document.getElementsByClassName("s");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 68) {
+       var x = document.getElementsByClassName("d");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 70) {
+       var x = document.getElementsByClassName("f");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 71) {
+       var x = document.getElementsByClassName("g");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 72) {
+       var x = document.getElementsByClassName("h");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 74) {
+       var x = document.getElementsByClassName("j");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 75) {
+       var x = document.getElementsByClassName("k");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 76) {
+       var x = document.getElementsByClassName("l");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 90) {
+       var x = document.getElementsByClassName("z");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 88) {
+       var x = document.getElementsByClassName("x");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 67) {
+       var x = document.getElementsByClassName("c");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 86) {
+       var x = document.getElementsByClassName("v");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 66) {
+       var x = document.getElementsByClassName("b");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 78) {
+       var x = document.getElementsByClassName("n");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 77) {
+       var x = document.getElementsByClassName("m");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+
+    else if(event.keyCode == 32) {
+       var x = document.getElementsByClassName("space");
+       x[0].style.backgroundColor = "white";
+       x[0].style.color = "black";
+    }
+});
+
+//////////////////KeyUp///////////////////
+document.addEventListener('keyup', function(event) {
+    if(event.keyCode == 81) {
+       var x = document.getElementsByClassName("q");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 87) {
+       var x = document.getElementsByClassName("w");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 69) {
+       var x = document.getElementsByClassName("e");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 82) {
+       var x = document.getElementsByClassName("r");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 84) {
+       var x = document.getElementsByClassName("t");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 89) {
+       var x = document.getElementsByClassName("y");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 85) {
+       var x = document.getElementsByClassName("u");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 73) {
+       var x = document.getElementsByClassName("i");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    } 
+
+    else if(event.keyCode == 79) {
+       var x = document.getElementsByClassName("o");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 80) {
+       var x = document.getElementsByClassName("p");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 65) {
+       var x = document.getElementsByClassName("a");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 83) {
+       var x = document.getElementsByClassName("s");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 68) {
+       var x = document.getElementsByClassName("d");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 70) {
+       var x = document.getElementsByClassName("f");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 71) {
+       var x = document.getElementsByClassName("g");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 72) {
+       var x = document.getElementsByClassName("h");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 74) {
+       var x = document.getElementsByClassName("j");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 75) {
+       var x = document.getElementsByClassName("k");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 76) {
+       var x = document.getElementsByClassName("l");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 90) {
+       var x = document.getElementsByClassName("z");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 88) {
+       var x = document.getElementsByClassName("x");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 67) {
+       var x = document.getElementsByClassName("c");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 86) {
+       var x = document.getElementsByClassName("v");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 66) {
+       var x = document.getElementsByClassName("b");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 78) {
+       var x = document.getElementsByClassName("n");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 77) {
+       var x = document.getElementsByClassName("m");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+
+    else if(event.keyCode == 32) {
+       var x = document.getElementsByClassName("space");
+       x[0].style.backgroundColor = "black";
+       x[0].style.color = "white";
+    }
+});
