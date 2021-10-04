2ce46d5be4dc573a875c5b75d3d427c7e9390f75
Luander <69652684+LuanderFarias@users.noreply.github.com>
2021-10-04 00:07:20 -0300

    Updates...

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
-
-/* Otherwise just put the config content (json): */
-
 particlesJS('particles-js',
   
   {
@@ -122,7 +109,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#b61924",
+      "background_color": "#181818",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
